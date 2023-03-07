8db934b50ec866e68d91c8031fd74fe315b9f15d
Reyhan <rcorek@gmail.com>
2023-03-07 23:03:33 +0300

    login step definitions

--- src/test/java/com/cydeo/step_definitions/LoginStepDefinitions.java
+++ src/test/java/com/cydeo/step_definitions/LoginStepDefinitions.java
@@ -1,43 +1,51 @@
 package com.cydeo.step_definitions;
 
 import io.cucumber.java.en.And;
+import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 
 public class LoginStepDefinitions {
 
-    @When("user enter librarian username")
-    public void user_enter_librarian_username() {
+    @When("user enters librarian username")
+    public void user_enters_librarian_username() {
         System.out.println("User enters librarian username");
     }
-    @When("users enters librarian password")
-    public void users_enters_librarian_password() {
+    @When("user enters librarian password")
+    public void user_enters_librarian_password() {
         System.out.println("User enters librarian password");
     }
     @Then("user should see the dashboard")
     public void user_should_see_the_dashboard() {
         System.out.println("User should see the dashboard");
+
     }
 
     @When("user enters student username")
-    public void user_enters_student_username() {
+    public void userEntersStudentUsername() {
         System.out.println("User enters student username");
     }
-    @When("user enters student password")
-    public void user_enters_student_password() {
+
+    @And("user enters student password")
+    public void userEntersStudentPassword() {
         System.out.println("User enters student password");
     }
 
-
-
-    @When("user enter admin username")
-    public void userEnterAdminUsername() {
-        System.out.println("User enter admin username");
+    @When("user enters admin username")
+    public void userEntersAdminUsername() {
+        System.out.println("user enters admin username");
     }
 
-    @And("user enter admin password")
-    public void userEnterAdminPassword() {
-        System.out.println("User enter admin password");
-
+    @And("user enters admin password")
+    public void userEntersAdminPassword() {
+        System.out.println("user enters admin password");
     }
+
+    @Given("user is on the library login page")
+    public void userIsOnTheLibraryLoginPage() {
+        System.out.println("user is on the library login page");
+    }
+
+
+
 }
